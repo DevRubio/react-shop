515710993fc1a0450c51096f092343b975a1f448
oscar.franco <ofrubio0@gmail.com>
2023-05-05 08:28:49 -0500

    Se organiza el menu desplegable de la App

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import '@styles/ProductItem.scss'
 
 import addToCard from '@icons/bt_add_to_cart.svg'
 
 const ProductItem =()=>{
+	const [cart, setCart] = useState([])
+	
+	const handleClik = ()=>{
+		setCart([])
+	}
+
     return(
         <div className="ProductItem">
 			<img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" />
@@ -12,9 +18,9 @@ const ProductItem =()=>{
 					<p>$120,00</p>
 					<p>Bike</p>
 				</div>
-				<figure>
+				<figure onClick={handleClik}>
 					<img src={addToCard} alt="addToCard" />
-				</figure>
+				</figure>				
 			</div>
 		</div>
     )
